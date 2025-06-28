e0b8b2ea5ee79db4a57912f104d02fdd3a4c2806
Max Konev <max@pushwoosh.com>
2025-06-28 15:40:42 +0700

    fix cacheable on push block

--- Web/Block/PushwooshWeb.php
+++ Web/Block/PushwooshWeb.php
@@ -13,27 +13,16 @@ class PushwooshWeb extends Template
      * @var \Pushwoosh\Web\Helper\Data
      */
     protected $helperData;
-    protected $objectFactory;
-    /**
-     * @var \Magento\Checkout\Model\Session
-     */
-    protected $_checkoutSession;
-
-    protected $_customerSession;
 
     public function __construct(
         Context $context,
         HelperData $helperData,
         ObjectManagerInterface $objectManager,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Customer\Model\Session $customerSession,
         array $data = []
     )
     {
         $this->helperData       = $helperData;
         $this->objectManager    = $objectManager;
-        $this->_checkoutSession = $checkoutSession;
-        $this->_customerSession = $customerSession;
 
         parent::__construct($context, $data);
     }
@@ -41,63 +30,5 @@ class PushwooshWeb extends Template
     public function getHelper()
     {
         return $this->helperData;
-    }
-
-    public function getOrder()
-    {
-        $order = $this->_checkoutSession->getLastRealOrder();
-
-        return $order;
-    }
-
-    public function cleanUpValue($str)
-    {
-        $str = trim(htmlspecialchars(strip_tags($str)));
-
-        return $str;
-
-    }
-
-    public function getOrderId()
-    {
-        return $this->getOrder()->getIncrementId();
-    }
-
-    public function getGrandTotal()
-    {
-        return $this->getOrder()->getGrandTotal();
-    }
-
-
-    public function getShippingAmount()
-    {
-        return $this->getOrder()->getShippingAmount();
-    }
-
-    public function getTaxAmount()
-    {
-        return $this->getOrder()->getTaxAmount();
-    }
-
-    public function getOrderStoreName()
-    {
-        $name = $this->cleanUpValue($this->getOrder()->getStoreName());
-        $name = preg_replace("/\r|\n/", "", $name);
-        return $name;
-    }
-
-    public function getOrderItems()
-    {
-        /** @Magento/Sales/Model/Order/Items */
-        return $this->getOrder()->getAllItems();
-    }
-
-    public function getCustomerEmail(): ?string
-    {
-        if ($this->_customerSession->isLoggedIn()) {
-            return $this->_customerSession->getCustomer()->getEmail();
-        }
-
-        return null;
     }
 }
